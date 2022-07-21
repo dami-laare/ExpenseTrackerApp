7a3a4f71d427d8fc4852ca376d849bac8604840d
Dev Nexus <osunkiyesitayo@gmail.com>
2022-07-21 23:33:05 +0100

    CONDITIONAL_INSTALL_BUTTON_RENDERING

--- src/components/UI/InstallButton.jsx
+++ src/components/UI/InstallButton.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { ImDownload } from "react-icons/im";
 
 const InstallButton = ({ onClick }) => {
-  return (
+  const [display, setDisplay] = useState(null);
+  function getPWADisplayMode() {
+    const isStandalone = window.matchMedia(
+      "(display-mode: standalone)"
+    ).matches;
+    if (document.referrer.startsWith("android-app://")) {
+      return "twa";
+    } else if (navigator.standalone || isStandalone) {
+      return "standalone";
+    }
+    return "browser";
+  }
+  useEffect(() => {
+    const displayMode = getPWADisplayMode();
+
+    setDisplay(displayMode);
+  }, []);
+  return display && display !== "standalone" ? (
     <Button
       variant="dark"
       style={{ position: "absolute", top: "1rem", right: "1rem" }}
@@ -12,6 +29,8 @@ const InstallButton = ({ onClick }) => {
       <ImDownload style={{ marginRight: "0.5rem" }} />
       <span>Install</span>
     </Button>
+  ) : (
+    ""
   );
 };
 
